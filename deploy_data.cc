4a72b88fd8c5b6e086daa3c0de88fefaba582eed
Robert Olson <olson@mcs.anl.gov>
2019-03-13 14:25:00 -0500

    Add deploy files

---
+++ deploy_data.cc
@@ -0,0 +1,7 @@
+
+#include <string>
+
+std::string deploy_app_service_url = APP_SERVICE_URL;
+std::string deploy_data_api_url = DATA_API_URL;
+std::string deploy_libdir = DEPLOY_LIBDIR;
+ 
---
+++ deploy_data.h
@@ -0,0 +1,6 @@
+
+#include <string>
+
+extern std::string deploy_data_api_url;
+extern std::string deploy_app_service_url;
+extern std::string deploy_libdir;
